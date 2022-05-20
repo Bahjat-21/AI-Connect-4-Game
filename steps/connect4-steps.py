c8b5a8dccabb23414bcb2f40448c753f62f7a212
Sanamjeet Meyer <sanamjeet.meyer@stud.th-deg.de>
2022-05-20 17:42:54 +0200

    Replace connect4-steps.py

--- steps/connect4-steps.py
+++ steps/connect4-steps.py
@@ -2,29 +2,27 @@
 from behave import when, then, given
 import pexpect
 
-@given("I start the program")
+@given("I start the game")
 def step_impl(context):
     image_name = "connect4:latest"
     context.proc = pexpect.spawn(f"docker run -i --rm {image_name}")
 
-@when("The board and game name appears")
+@when("The board and the game name appears")
 def step_impl(context):
-    context.proc.expect( "###############################\n"
-                         "######### CONNECT 4 ###########\n"
-                         "###############################")
+    context.proc.expect( "###############################")
 
-@when("I choose {player}")
+@when("I choose {player} as my player")
 def step_impl(context,player):
     context.proc.expect("X oder O:")
     context.proc.sendline(player)
 
-@when("I enter {pos}")
+@when("I enter column {pos}")
 def step_impl(context,pos):
     context.proc.expect("Your move:")
     context.proc.sendline(pos)
 
-@then("I need to enter next move")
+@then("I check the row a")
 def step_impl(context):
-    context.proc.expect("Your move")
+    context.proc.expect("a | X | X |   |   |   |   |   |")
 
 
